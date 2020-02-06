2a5571f892e04e795aa430caf041068b29ffbb25
ifeniprun16 <ifeniprun16@gmail.com>
2020-02-06 16:09:15 +0700

    belum jadi

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Guestbook | Daspro Walimatul Project 2020</title>
+    <title>Checkin Form | International Workshop</title>
     <link rel="stylesheet" href="static/css/bootstrap.min.css">
     <link rel="stylesheet" href="static/css/style.css" type="text/css">
     <link href="static/css/sweetalert2.css" rel="stylesheet">
 </head>
     <!------ Include the above in your HEAD tag ---------->
 
-<body onload="sweetalertclick()">
-    <div class="wrapper fadeInDown">
+<body onload="sweetalertclick()" style="background-image: url(./static/img/bg.png)">
+    <div class="wrapper fadeInDown" style="background: none !important">
         <div id="formContent">
             <!-- Icon -->
             <div class="fadeIn first">
@@ -17,7 +17,7 @@
             </div>
             <!-- Login Form -->
             <form action="input-aksi.php" method="post">
-                <input type="number" name="nim" class="fadeIn second" id="nim" placeholder="NIM" required="">
+                <input type="number" name="nim" class="fadeIn second" id="nim" placeholder="NIM / NIDN" required="">
                 <input type="text" name="nama" class="fadeIn second" id="nama" placeholder="Nama Lengkap (akan tertulis di sertifikat)" required="">
                 <input type="email" name="email" class="fadeIn third" id="email" placeholder="Email" required="">
                 <input type="submit" class="fadeIn fourth" value="Submit" name="submit">
@@ -29,13 +29,12 @@
     <script src="static/js/bootstrap.min.js"></script>
     <script src="static/js/main.js"></script>
     <script src="static/js/sweetalert2.all.min.js"></script>
-
     <script>
     function sweetalertclick() {
         Swal.fire({
         icon: 'success',
         title: 'Halo, '+'<?php echo $_GET["name"] ?>',
-        text: 'Selamat datang di Syukuran Keprofesian DASPRO 2018!',
+        text: 'Selamat datang di International Workshop Data Science!',
         timer: 5000
         })
     }
